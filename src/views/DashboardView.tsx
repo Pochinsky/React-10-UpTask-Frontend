c72922296f2ddae223c4a72beb509b375a560f01
Pochinsky <tomas.guttman@sansano.usm.cl>
2025-07-09 22:15:17 -0400

    🎉 project initializated

---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function DashboardView() {
+  return (
+    <>
+      <h1 className="text-5xl font-black">Mis Proyectos</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">
+        Administra tus proyectos
+      </p>
+      <nav className="my-6">
+        <Link
+          to="/projects/create"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+        >
+          Crear proyecto
+        </Link>
+      </nav>
+    </>
+  );
+}
