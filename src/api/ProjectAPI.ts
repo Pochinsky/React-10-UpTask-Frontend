1e92ce778c6039a43b6629e4deb9a4dc6cf101bf
Pochinsky <tomas.guttman@sansano.usm.cl>
2025-08-01 02:00:09 -0400

    ✨ create project added

---
+++ src/api/ProjectAPI.ts
@@ -0,0 +1,13 @@
+import { isAxiosError } from "axios";
+import api from "@/lib/axios";
+import type { ProjectFormData } from "@/types/index";
+
+export async function createProject(formData: ProjectFormData) {
+  try {
+    const { data } = await api.post("/projects", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response)
+      throw new Error(error.response.data.error);
+  }
+}
---
+++ src/components/projects/ProjectForm.tsx
@@ -0,0 +1,70 @@
+import ErrorMessage from "@/components/ErrorMessage";
+import type { FieldErrors, UseFormRegister } from "react-hook-form";
+import type { ProjectFormData } from "types";
+
+type ProjectFormProps = {
+  register: UseFormRegister<ProjectFormData>;
+  errors: FieldErrors<ProjectFormData>;
+};
+
+export default function ProjectForm({ register, errors }: ProjectFormProps) {
+  return (
+    <>
+      <div className="mb-5 space-y-3">
+        <label htmlFor="projectName" className="text-sm uppercase font-bold">
+          Nombre del proyecto
+        </label>
+        <input
+          id="projectName"
+          className="w-full p-3  border border-gray-200"
+          type="text"
+          placeholder="Ej: CRM"
+          {...register("projectName", {
+            required: "El nombre del proyecto es obligatorio",
+          })}
+        />
+
+        {errors.projectName && (
+          <ErrorMessage>{errors.projectName.message}</ErrorMessage>
+        )}
+      </div>
+
+      <div className="mb-5 space-y-3">
+        <label htmlFor="clientName" className="text-sm uppercase font-bold">
+          Nombre del cliente
+        </label>
+        <input
+          id="clientName"
+          className="w-full p-3  border border-gray-200"
+          type="text"
+          placeholder="Ej: Distribuidora SPA"
+          {...register("clientName", {
+            required: "El nombre del cliente es obligatorio",
+          })}
+        />
+
+        {errors.clientName && (
+          <ErrorMessage>{errors.clientName.message}</ErrorMessage>
+        )}
+      </div>
+
+      <div className="mb-5 space-y-3">
+        <label htmlFor="description" className="text-sm uppercase font-bold">
+          Descripción
+        </label>
+        <textarea
+          id="description"
+          className="w-full p-3  border border-gray-200"
+          placeholder="Describa las principales características del proyecto"
+          {...register("description", {
+            required: "La descripción del proyecto es obligatoria",
+          })}
+        />
+
+        {errors.description && (
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
+        )}
+      </div>
+    </>
+  );
+}
